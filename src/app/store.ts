112e4094386e7ff00a0856ace64c00aceed92c98
Inna3112 <innula3112@gmail.com>
2021-10-22 21:55:39 +0300

    saga settings added in store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,7 +3,9 @@ import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from './app-reducer'
-import {authReducer} from "../features/Login/auth-reducer";
+import {authReducer} from '../features/Login/auth-reducer'
+import createSagaMiddleware from 'redux-saga'
+import {takeEvery} from 'redux-saga/effects'
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -13,10 +15,29 @@ const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer
 })
+
+const sagaMiddleware = createSagaMiddleware()
+
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, sagaMiddleware));
+
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
+sagaMiddleware.run(rootWatcher)
+
+function* rootWatcher(){
+    alert('rootWatcher')
+    yield takeEvery('ActivatorActionType', rootWorker)
+}
+function* rootWorker(){
+    alert('rootWorker')
+}
+
+setTimeout(() => {
+    //@ts-ignore
+    store.dispatch({type: 'ActivatorActionType'})
+}, 2000)
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
