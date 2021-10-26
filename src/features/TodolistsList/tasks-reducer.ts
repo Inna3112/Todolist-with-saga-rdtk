5c63edb6463d7ced74946e39b0d2893d6b0946e7
Inna3112 <innula3112@gmail.com>
2021-10-26 17:05:52 +0300

    fetchTasksTC, removeTaskTC created with createAsyncThunk

--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -13,81 +13,40 @@ import {Dispatch} from 'redux'
 import {AppRootStateType} from '../../app/store'
 import {setAppStatusAC} from '../../app/app-reducer'
 import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 const initialState: TasksStateType = {}
-const slice = createSlice({
-    name: 'tasks',
-    initialState: initialState,
-    // здесь выполняют функцию редюсеров и создают экшены
-    reducers: {
-        removeTaskAC(state, action: PayloadAction<{taskId: string, todolistId: string}>){
-            const tasks = state[action.payload.todolistId]
-            const index = tasks.findIndex(t => t.id === action.payload.taskId)
-            if(index > -1){
-                tasks.splice(index, 1)
-            }
-        },
-        addTaskAC(state, action: PayloadAction<{task: TaskType}>){
-            state[action.payload.task.todoListId].unshift(action.payload.task)
-        },
-        updateTaskAC(state, action: PayloadAction<{taskId: string, model: UpdateDomainTaskModelType, todolistId: string}>){
-            const tasks = state[action.payload.todolistId]
-            const index = tasks.findIndex(t => t.id === action.payload.taskId)
-            if(index > -1){
-                tasks[index] = {...tasks[index], ...action.payload.model}
-            }
-        },
-        setTasksAC(state, action: PayloadAction<{tasks: Array<TaskType>, todolistId: string}>){
-            state[action.payload.todolistId] = action.payload.tasks
-        },
-    },
-    // нужны тогда, когда не нужно создавать экшен - он уже создат в todolistReducer, а нужно выполнить функцию редюсера
-    //можно обработать АС написанные вручную здесь
 
-    //можно передать обьектом, но тогда нужно будет выносить типизацию екшина вручную
-    // extraReducers: {
-    // [addTodolistAC.type](state, action: PayloadAction<{}>){},
-    // [removeTodolistAC.type](state, action: PayloadAction<{}>){},
-    // [setTodolistsAC.type](state, action: PayloadAction<{}>){},
-    // }
-    //поэтому:
-    extraReducers: (builder) => {
-        builder.addCase(addTodolistAC, (state, action) => {
-            state[action.payload.todolist.id] = []
-        });
-        builder.addCase(removeTodolistAC, (state, action) => {
-            delete state[action.payload.id]
-        });
-        builder.addCase(setTodolistsAC, (state, action) => {
-            action.payload.todolists.forEach((tl: any) => {
-                state[tl.id] = []
-            })
-        });
-    }
-})
-
-export const tasksReducer = slice.reducer
-export const {removeTaskAC, addTaskAC, updateTaskAC, setTasksAC} = slice.actions
-
-
-// thunks
-export const fetchTasksTC = (todolistId: string) => (dispatch: Dispatch) => {
-    dispatch(setAppStatusAC({status: 'loading'}))
-    todolistsAPI.getTasks(todolistId)
+//данные в санк нужно передавать обьектом пейлоад, если их несколько
+//thunkAPI - это диспатч и гетстейт
+//ВЫГОДА ТАКОЙ САНК: создается 2 екшена - один выполнится при успешном выполнении запроса на сервер, второй при ошибке
+interface FetchTasksData {
+    tasks: TaskType[],
+    todolistId: string
+}
+export const fetchTasksTC = createAsyncThunk<FetchTasksData, string>('tasks/fetchTasks', (todolistId: string, thunkAPI) => {
+    thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
+    return todolistsAPI.getTasks(todolistId)
         .then((res) => {
             const tasks = res.data.items
-            dispatch(setTasksAC({tasks, todolistId}))
-            dispatch(setAppStatusAC({status: 'succeeded'}))
+            // thunkAPI.dispatch(setTasksAC({tasks, todolistId}))
+            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+            return {tasks, todolistId} as FetchTasksData
         })
+})
+
+interface RemoveTaskData{
+    taskId: string,
+    todolistId: string
 }
-export const removeTaskTC = (taskId: string, todolistId: string) => (dispatch: Dispatch) => {
-    todolistsAPI.deleteTask(todolistId, taskId)
-        .then(res => {
-            const action = removeTaskAC({taskId, todolistId})
-            dispatch(action)
-        })
-}
+export const removeTaskTC = createAsyncThunk<RemoveTaskData, RemoveTaskData>('tasks/removeTask', (param: {taskId: string, todolistId: string}, thunkAPI) => {
+    return todolistsAPI.deleteTask(param.todolistId, param.taskId)
+        .then(res => ({taskId: param.taskId, todolistId: param.todolistId} as RemoveTaskData))
+})
+
+
+
+
 export const addTaskTC = (title: string, todolistId: string) => (dispatch: Dispatch) => {
     dispatch(setAppStatusAC({status: 'loading'}))
     todolistsAPI.createTask(todolistId, title)
@@ -138,6 +97,74 @@ export const updateTaskTC = (taskId: string, domainModel: UpdateDomainTaskModelT
                 handleServerNetworkError(error, dispatch);
             })
     }
+
+const slice = createSlice({
+    name: 'tasks',
+    initialState: initialState,
+    // здесь выполняют функцию редюсеров и создают экшены
+    reducers: {
+        // removeTaskAC(state, action: PayloadAction<{taskId: string, todolistId: string}>){
+        //     const tasks = state[action.payload.todolistId]
+        //     const index = tasks.findIndex(t => t.id === action.payload.taskId)
+        //     if(index > -1){
+        //         tasks.splice(index, 1)
+        //     }
+        // },
+        addTaskAC(state, action: PayloadAction<{task: TaskType}>){
+            state[action.payload.task.todoListId].unshift(action.payload.task)
+        },
+        updateTaskAC(state, action: PayloadAction<{taskId: string, model: UpdateDomainTaskModelType, todolistId: string}>){
+            const tasks = state[action.payload.todolistId]
+            const index = tasks.findIndex(t => t.id === action.payload.taskId)
+            if(index > -1){
+                tasks[index] = {...tasks[index], ...action.payload.model}
+            }
+        },
+        // setTasksAC(state, action: PayloadAction<{tasks: Array<TaskType>, todolistId: string}>){
+        //     state[action.payload.todolistId] = action.payload.tasks
+        // },
+    },
+    // нужны тогда, когда не нужно создавать экшен - он уже создат в todolistReducer, а нужно выполнить функцию редюсера
+    //можно обработать АС написанные вручную здесь
+
+    //можно передать обьектом, но тогда нужно будет выносить типизацию екшина вручную
+    // extraReducers: {
+    // [addTodolistAC.type](state, action: PayloadAction<{}>){},
+    // [removeTodolistAC.type](state, action: PayloadAction<{}>){},
+    // [setTodolistsAC.type](state, action: PayloadAction<{}>){},
+    // }
+    //поэтому:
+    extraReducers: (builder) => {
+        builder.addCase(addTodolistAC, (state, action) => {
+            state[action.payload.todolist.id] = []
+        });
+        builder.addCase(removeTodolistAC, (state, action) => {
+            delete state[action.payload.id]
+        });
+        builder.addCase(setTodolistsAC, (state, action) => {
+            action.payload.todolists.forEach((tl: any) => {
+                state[tl.id] = []
+            })
+        });
+        builder.addCase(fetchTasksTC.fulfilled, (state, action) => {
+            state[action.payload.todolistId] = action.payload.tasks
+        });
+        builder.addCase(removeTaskTC.fulfilled, (state, action) => {
+            const tasks = state[action.payload.todolistId]
+            const index = tasks.findIndex(t => t.id === action.payload.taskId)
+            if(index > -1){
+                tasks.splice(index, 1)
+            }
+        });
+    }
+})
+
+export const tasksReducer = slice.reducer
+export const {addTaskAC, updateTaskAC} = slice.actions
+
+
+// thunks
+
 
 // types
 export type UpdateDomainTaskModelType = {
